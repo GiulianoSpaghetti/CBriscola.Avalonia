94fe1ffa25fe4c2709e3914daa0db593230729b7
Giulio Sorrentino <numerone@fastwebnet.it>
2023-11-10 20:12:56 +0100

    ora la mano è ridimensionabile in base alle esigenze

--- CBriscola.Avalonia/giocatore.cs
+++ CBriscola.Avalonia/giocatore.cs
@@ -17,21 +17,20 @@ namespace org.altervista.numerone.framework
 		private string nome;
 		private Carta[] mano;
 		private bool ordinaMano;
-		private UInt16 numeroCarte;
-		private UInt16 iCarta;
-		private UInt16 iCartaGiocata;
-		private UInt16 punteggio;
+		private UInt16 numeroCarte, iCarta, iCartaGiocata, punteggio;
+		private readonly UInt16 totaleCarte;
 		private readonly GiocatoreHelper helper;
-		public enum Carta_GIOCATA { NESSUNA_Carta_GIOCATA = UInt16.MaxValue };
+		public enum CARTA_GIOCATA { NESSUNA_CARTA_GIOCATA = UInt16.MaxValue };
 		public Giocatore(GiocatoreHelper h, string n, UInt16 carte, bool ordina = true)
 		{
+			totaleCarte = carte;
 			ordinaMano = ordina;
 			numeroCarte = carte;
-			iCartaGiocata = (UInt16)(Carta_GIOCATA.NESSUNA_Carta_GIOCATA);
+			iCartaGiocata = (UInt16)(CARTA_GIOCATA.NESSUNA_CARTA_GIOCATA);
 			punteggio = 0;
 			helper = h;
 			nome = n;
-			mano = new Carta[3];
+			mano = new Carta[totaleCarte];
 			iCarta = 0;
 		}
 		public string GetNome() { return nome; }
@@ -42,14 +41,14 @@ namespace org.altervista.numerone.framework
 		{
 			UInt16 i = 0;
 			Carta temp;
-			if (iCarta == numeroCarte && iCartaGiocata == (UInt16)Carta_GIOCATA.NESSUNA_Carta_GIOCATA)
+			if (iCarta == numeroCarte && iCartaGiocata == (UInt16)CARTA_GIOCATA.NESSUNA_CARTA_GIOCATA)
 				throw new ArgumentException($"Chiamato Giocatore::setCarta con mano.size()==numeroCarte=={numeroCarte}");
-			if (iCartaGiocata != (UInt16)Carta_GIOCATA.NESSUNA_Carta_GIOCATA)
+			if (iCartaGiocata != (UInt16)CARTA_GIOCATA.NESSUNA_CARTA_GIOCATA)
 			{
 				for (i = iCartaGiocata; i < numeroCarte - 1; i++)
 					mano[i] = mano[i + 1];
 				mano[i] = null;
-				iCartaGiocata = (UInt16)Carta_GIOCATA.NESSUNA_Carta_GIOCATA;
+				iCartaGiocata = (UInt16)CARTA_GIOCATA.NESSUNA_CARTA_GIOCATA;
 				mano[iCarta - 1] = SostituisciCartaGiocata(m);
 				for (i = (UInt16)(iCarta - 2); i < UInt16.MaxValue && iCarta > 1 && mano[i].CompareTo(mano[i + 1]) < 0; i--)
 				{
