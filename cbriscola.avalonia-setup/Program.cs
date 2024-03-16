15656cf168006fdca31b92314af36435e7ad8700
Giulio Sorrentino <numerone@fastwebnet.it>
2024-03-16 18:53:29 +0100

    Add files via upload

--- cbriscola.avalonia-setup/Program.cs
+++ cbriscola.avalonia-setup/Program.cs
@@ -35,7 +35,7 @@ namespace cbriscola.avalonia_setup
             );
 
             project.GUID = new Guid("68B61DE0-07A0-499E-B3FB-F15873641EB4");
-            project.Version = new Version("0.7.8");
+            project.Version = new Version("0.8.9");
             project.Platform = Platform.x64;
             project.SourceBaseDir = "E:\\source\\CBriscola.Avalonia\\CBriscola.Avalonia\\bin\\Release\\net8.0-windows10.0.22621.0";
             project.LicenceFile = "LICENSE.rtf";
