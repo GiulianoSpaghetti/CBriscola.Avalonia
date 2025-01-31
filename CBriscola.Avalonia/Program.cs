e9c1f2295b786dc0404c1abd2281244fa28ca9fe
Giulio Sorrentino <numerone@fastwebnet.it>
2025-01-31 07:52:14 +0100

    ora ve la sreastradò

--- CBriscola.Avalonia/Program.cs
+++ CBriscola.Avalonia/Program.cs
@@ -9,7 +9,7 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) =>la rBuildAvaloniaApp()
+    public static void Main(string[] args) => BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
